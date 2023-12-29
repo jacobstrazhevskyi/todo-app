48d23cc82fd9888e114923907690dd5c3a195657
jacob.strazhevskyi <jacob.strazhevskyi@gmail.com>
2023-12-29 23:26:05 +0200

    HomePage wrapped around Container

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,8 +1,10 @@
-import { Typography } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import React from 'react';
 
 export const HomePage: React.FC = () => {
   return (
-    <Typography>HOME PAGE PAGE HOME HOME HOME PAGE HOME PAGE HOME</Typography>
+    <Container>
+      <Typography>HOME PAGE PAGE HOME HOME HOME PAGE HOME PAGE HOME</Typography>
+    </Container>
   );
 };
