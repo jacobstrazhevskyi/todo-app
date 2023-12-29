89a2b46bfcf3442770d4a8db5f1643e0d852dcfe
jacob.strazhevskyi <jacob.strazhevskyi@gmail.com>
2023-12-29 23:25:26 +0200

    implemented setting theme from storage

--- src/components/MuiThemeWrapper.tsx
+++ src/components/MuiThemeWrapper.tsx
@@ -1,31 +1,10 @@
-import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import React from 'react';
 import App from './App';
+import { useAppSelector } from '../app/hooks';
 
 export const MuiThemeWrapper: React.FC = () => {
-  const theme = createTheme({
-    palette: {
-      mode: 'dark',
-      primary: {
-        main: '#373737',
-      },
-      secondary: {
-        main: '#221266',
-      },
-      background: {
-        default: '#121212',
-      },
-      text: {
-        primary: '#ffffff',
-        secondary: '#6b675a',
-        disabled: '#b81d25',
-      },
-    },
-    typography: {
-      fontFamily: 'Roboto, sans-serif',
-      fontSize: 16,
-    },
-  });
+  const theme = useAppSelector(state => state.theme);
 
   return (
     <ThemeProvider theme={theme}>
