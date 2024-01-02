a3baf51dbc6fb0c60fe78cbfc50b55046876a9f2
jacob.strazhevskyi <jacob.strazhevskyi@gmail.com>
2024-01-02 13:17:05 +0200

    added some styling

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,10 +1,29 @@
-import { Container, Typography } from '@mui/material';
+import {
+  Container,
+  Paper,
+  Typography,
+  styled,
+  useTheme,
+} from '@mui/material';
 import React from 'react';
 
 export const HomePage: React.FC = () => {
+  const theme = useTheme();
+
+  const BoxStyledPaper = styled(Paper)({
+    width: '100%',
+    height: '100%',
+    backgroundColor: theme.palette.primary.main,
+    padding: 10,
+  });
+
   return (
     <Container>
-      <Typography>HOME PAGE PAGE HOME HOME HOME PAGE HOME PAGE HOME</Typography>
+      <BoxStyledPaper elevation={5}>
+        <Typography>
+          HOME PAGE PAGE HOME HOME HOME PAGE HOME PAGE HOME
+        </Typography>
+      </BoxStyledPaper>
     </Container>
   );
 };
