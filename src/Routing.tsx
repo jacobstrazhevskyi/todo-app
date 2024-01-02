41bf9215f01c079f9ef7529511cdb887f521a722
jacob.strazhevskyi <jacob.strazhevskyi@gmail.com>
2024-01-02 13:13:41 +0200

    added redirecting from "/" to "/home"

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import {
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from 'react-router-dom';
 import { Navigation } from './components/Navigation';
 import { NotFoundPage } from './components/NotFoundPage';
 import { HomePage } from './components/HomePage';
 
 export const Routing: React.FC = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (pathname === '/') {
+      navigate('/home');
+    }
+  }, [navigate, pathname]);
+
   return (
     <>
       <Navigation />
