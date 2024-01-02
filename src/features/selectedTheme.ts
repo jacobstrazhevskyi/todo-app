d2ee62013d071c4c5ac79c3d9d9f9ef0409f4269
jacob.strazhevskyi <jacob.strazhevskyi@gmail.com>
2024-01-02 13:15:33 +0200

    redirected themes from reducer to another file

--- src/features/selectedTheme.ts
+++ src/features/selectedTheme.ts
@@ -1,89 +1,9 @@
-/* eslint-disable no-param-reassign */
-import { Theme, ThemeOptions, createTheme } from '@mui/material';
+import { Theme, createTheme } from '@mui/material';
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { themes } from '../utils/themes';
 
 const OSTheme = window
   .matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-
-type ThemeObject = {
-  themeName: string,
-  themeObject: ThemeOptions,
-};
-
-export const themes: ThemeObject[] = [
-  {
-    themeName: 'dark',
-    themeObject: {
-      palette: {
-        mode: 'dark',
-        common: {
-          white: '#ffffff',
-          black: '#000000',
-        },
-        primary: {
-          main: '#373737',
-          light: '#5f5f5f',
-          dark: '#262626',
-          contrastText: '#ffffff',
-        },
-        secondary: {
-          main: '#221266',
-          dark: '#170c47',
-          light: '#4e4184',
-        },
-        background: {
-          default: '#121212',
-          paper: '#121212',
-        },
-        text: {
-          primary: '#ffffff',
-          secondary: '#6b675a',
-          disabled: '#b81d25',
-        },
-      },
-      typography: {
-        fontFamily: 'Roboto, sans-serif',
-        fontSize: 16,
-      },
-    },
-  },
-  {
-    themeName: 'light',
-    themeObject: {
-      palette: {
-        mode: 'light',
-        common: {
-          white: '#ffffff',
-          black: '#000000',
-        },
-        primary: {
-          main: '#673ab7',
-          dark: '#482880',
-          light: '#482880',
-          contrastText: '#ffffff',
-        },
-        secondary: {
-          main: '#651fff',
-          dark: '#4615b2',
-          light: '#834bff',
-        },
-        text: {
-          primary: '#000000',
-          secondary: '#291749',
-          disabled: '#6b675a',
-        },
-        background: {
-          default: '#ffffff',
-          paper: '#ffffff',
-        },
-      },
-      typography: {
-        fontFamily: 'Roboto, sans-serif',
-        fontSize: 16,
-      },
-    },
-  },
-];
 
 const getTheme = (themeName: string): Theme => {
   const theme = themes.find((foundTheme) => foundTheme.themeName === themeName)
@@ -92,10 +12,10 @@ const getTheme = (themeName: string): Theme => {
   return createTheme(theme.themeObject);
 };
 
-const themeFromLocalStorage = localStorage.getItem('theme');
+const themeNameFromLocalStorage = localStorage.getItem('theme');
 
-const initialState: Theme = themeFromLocalStorage
-  ? getTheme(themeFromLocalStorage)
+const initialState: Theme = themeNameFromLocalStorage
+  ? getTheme(themeNameFromLocalStorage)
   : getTheme(OSTheme);
 
 const selectedThemeSlice = createSlice({
